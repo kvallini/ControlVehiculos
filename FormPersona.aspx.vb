79fdf69a6a15e5db329be7a2b119b6c6a5ddfe39
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-10-14 21:22:47 -0600

    Agregar archivos de proyecto.

---
+++ FormPersona.aspx.vb
@@ -0,0 +1,104 @@
+﻿Public Class FormPersona
+    Inherits System.Web.UI.Page
+    Public persona As New Persona()
+    Protected dbHelper As New dbPersona()
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btn_guardar(sender As Object, e As EventArgs)
+
+        persona.Nombre = txtNombre.Text
+        persona.Apellido1 = txtApellido.Text
+        persona.Apellido2 = txtApellido.Text
+        persona.FechaNacimiento = txtEdad.Text
+
+        If dbHelper.create(persona) Then
+            lblMensaje.Text = "Persona creada"
+            txtNombre.Text = ""
+            txtApellido.Text = ""
+            txtEdad.Text = ""
+        Else
+            lblMensaje.Text = "Ocurrio un error"
+        End If
+
+        gvPersonas.DataBind()
+
+    End Sub
+
+    Protected Sub gvPersonas_RowDeleting(sender As Object, e As GridViewDeleteEventArgs)
+
+        Try
+            Dim id As Integer = Convert.ToInt32(gvPersonas.DataKeys(e.RowIndex).Value)
+            dbHelper.delete(id)
+            e.Cancel = True
+            gvPersonas.DataBind()
+        Catch ex As Exception
+            lblMensaje.Text = "Error al eliminar la persona: " & ex.Message
+        End Try
+
+    End Sub
+
+    Protected Sub gvPersonas_RowEditing(sender As Object, e As GridViewEditEventArgs)
+
+
+
+    End Sub
+
+    Protected Sub gvPersonas_RowCancelingEdit(sender As Object, e As GridViewCancelEditEventArgs)
+
+        gvPersonas.EditIndex = -1
+        gvPersonas.DataBind()
+
+
+    End Sub
+
+    Protected Sub gvPersonas_RowUpdating(sender As Object, e As GridViewUpdateEventArgs)
+
+
+        Dim id As Integer = Convert.ToInt32(gvPersonas.DataKeys(e.RowIndex).Value)
+        Dim persona As Persona = New Persona With {
+            .Nombre = e.NewValues("Nombre"),
+            .Apellido1 = e.NewValues("Apellido"),
+            .Apellido2 = e.NewValues("Apellido"),
+            .FechaNacimiento = e.NewValues("Edad"),
+            .IdPersona = id
+        }
+        dbHelper.update(persona)
+        gvPersonas.DataBind()
+        e.Cancel = True
+        gvPersonas.EditIndex = -1
+
+    End Sub
+
+    Protected Sub gvPersonas_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+        Dim row As GridViewRow = gvPersonas.SelectedRow()
+        Dim id As Integer = Convert.ToInt32(row.Cells(2).Text)
+        Dim persona As Persona = New Persona()
+
+        txtNombre.Text = row.Cells(3).Text
+        txtApellido.Text = row.Cells(4).Text
+        txtEdad.Text = row.Cells(5).Text
+
+        editando.Value = id
+
+    End Sub
+
+    Protected Sub btnActualizar_Click(sender As Object, e As EventArgs)
+
+
+        Dim persona As Persona = New Persona With {
+            .Nombre = txtNombre.Text(),
+            .Apellido1 = txtApellido.Text(),
+            .Apellido2 = txtApellido.Text(),
+            .FechaNacimiento = txtEdad.Text(),
+            .IdPersona = editando.Value()
+        }
+        dbHelper.update(persona)
+        gvPersonas.DataBind()
+        gvPersonas.EditIndex = -1
+
+    End Sub
+End Class
